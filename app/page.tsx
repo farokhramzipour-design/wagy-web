21892c2d5d22f4fe8b23b5be91d0dd6835f4839e
behnam <behnam@behnams-MacBook-Pro.local>
2026-02-15 13:39:06 -0800

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import { LandingPageSections } from "@/components/sections/landing-page";
 
+export const dynamic = "force-dynamic";
+
 export default function HomePage() {
   return <LandingPageSections />;
 }
